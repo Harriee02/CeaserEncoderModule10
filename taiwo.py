c65a776731cedeec3904a130555138e56b0fedca
Harriee02 <62918145+Harriee02@users.noreply.github.com>
2021-04-20 10:19:55 +0100

    Update taiwo.py

--- taiwo.py
+++ taiwo.py
@@ -14,11 +14,15 @@ def CeaserEncoder(shift):
       else:
         another_str += chr(ord(j)+shift)
     counter = 5
+    new_line_counter = 0
     for q in range(0,len(another_str),5):
         new_new += another_str[q:counter]
         new_new += " "
         counter += 5
+        new_line_counter += 1
+        if not new_line_counter % 10:
+            new_new += "/n"
     return new_new
     
-a = int(sys.argv[1])
-print(CeaserEncoder(a))
+q = int(sys.argv[1])
+print(CeaserEncoder(q))
